fa45fb8636b479e635ec7717384bbf8f4b96f213
vdi-terminal <test@test>
2024-11-10 09:32:54 +0100

    Download project into this one

---
+++ src/Patient.java
@@ -0,0 +1,28 @@
+import java.time.LocalDateTime;
+
+public class Patient {
+    public String firstName;
+    public String lastName;
+    public int id;
+    public String dateOfBirth;
+    public int age;
+    public int phoneNumber;
+    public String mailAddress;
+
+    //konstruktor domyślny
+    public Patient() {
+
+    }
+
+    //konstruktor zmyślny xd
+    public Patient(String firstName, String lastName, int id, String dateOfBirth, int age, int phoneNumber, String mailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.dateOfBirth = dateOfBirth;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.mailAddress = mailAddress;
+    }
+
+}
